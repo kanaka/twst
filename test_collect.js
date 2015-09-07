a14e6b94b4aef060de66480c45a23f5bafcdd343
Joel Martin <github@martintribe.org>
2015-09-06 23:31:02 -0500

    Add collect implementation and test.

---
+++ test_collect.js
@@ -0,0 +1,15 @@
+#!/usr/bin/env node
+var Twst = require('./twst').Twst,
+    twst = new Twst();
+
+function test_func() {
+    return location.search + Math.random();
+}
+
+setInterval(function() {
+    var opts = {timeout: 10000,
+                restype: 'return'};
+    twst.collect(test_func, opts, function(result, data) {
+        console.log('collect result:', result, data);
+    });
+}, 6000);
--- twst.js
+++ twst.js
@@ -13,6 +13,12 @@
 var express = require('express'),
     express_ws = require('express-ws');
 
+function keysEqual(obj1, obj2) {
+    var ks1 = JSON.stringify(Object.keys(obj1).sort()),
+        ks2 = JSON.stringify(Object.keys(obj2).sort());
+    return ks1 === ks2;
+}
+
 function Twst(opts) {
     if (!opts) { opts = {} }
     if (!opts.port) { opts.port = 9000 }
@@ -22,6 +28,7 @@ function Twst(opts) {
     this.clients = {};
     this._nextClientIdx = 0;
     this._broadcastIdx = 0;
+    this._collectCtx = {};
     this._onmessage = null;
     this._onclose = null;
 
@@ -34,14 +41,33 @@ function Twst(opts) {
         var clientIdx = self._nextClientIdx++;
         self.clients[clientIdx] = ws;
         console.log(clientIdx + ': New client');
-        ws.on('message', function(msg) {
-            console.log(clientIdx + ': received: "%s"', msg);
+
+        ws.on('message', function(raw) {
+            console.log(clientIdx + ': received: \'%s\'', raw);
+            var msg = JSON.parse(raw);
             if (self._onmessage) { self._onmessage(clientIdx, msg); }
+
+            // Handle collect
+            var id = msg.id;
+            if (id in self._collectCtx) {
+                var ctx = self._collectCtx[id];
+                if (ctx.restype === msg.type) {
+                    ctx.data[clientIdx] = msg;
+                }
+                if (keysEqual(ctx.data, self.clients)) {
+                    var data = ctx.data,
+                        callback = ctx.callback;
+                    clearTimeout(ctx.timer);
+                    delete self._collectCtx[id];
+                    callback(true, data);
+                }
+            }
         });
+
         ws.on('close', function() {
             console.log(clientIdx + ': closed client');
             if (self._onclose) { self._onclose(clientIdx); }
-            delete clients[clientIdx];
+            delete self.clients[clientIdx];
         });
     });
 
@@ -49,18 +75,56 @@ function Twst(opts) {
     console.log('Server listening on :' + opts.port);
 }
 
-Twst.prototype.broadcast = function(type, data) {
-    if (typeof data === 'function') {
-        data = data.toString() + ";" + data.name + "();"
+// broadcast: send a message to every client
+//     opts.type: type of message to send
+Twst.prototype.broadcast = function(source, opts) {
+    // Defaults
+    if (!opts)      { opts = {}; }
+    if (!opts.type) { opts.type = "eval"; }
+    // Send function source over and run them
+    if (typeof source === 'function') {
+        source = source.toString() + ";" + source.name + "();"
     }
     var idxes = Object.keys(this.clients),
         msg = {id:    this._broadcastIdx++,
-               type:  type,
-               data:  data},
+               type:  opts.type,
+               data:  source},
         json = JSON.stringify(msg);
     for(var i=0; i<idxes.length; i++) {
         this.clients[idxes[i]].send(json);
     }
+}
+
+// collect: call broadcast, then call callback when all clients return
+// results or fail after opts.timeout.
+//     opts.timeout: zero or null for no timeout
+//     opts.restype: wait for 'return' or 'callback' for all clients.
+Twst.prototype.collect = function(source, opts, callback) {
+    // Defaults
+    if (!opts)         { opts = {}; }
+    if (!opts.timeout) { opts.timeout = null; }
+    if (!opts.type)    { opts.type = "eval"; }
+    if (!opts.restype) { opts.restype = "return"; }
+
+    if (Object.keys(this.clients).length === 0) {
+        // No clients to collect from
+        callback(true, {});
+        return;
+    }
+
+    var cid = this._broadcastIdx;
+    this._collectCtx[cid] = {callback: callback,
+                             restype:  opts.restype,
+                             data:     {}};
+    if (opts.timeout) {
+        var self = this;
+        this._collectCtx[cid].timer = setTimeout(function() {
+            var data = self._collectCtx[cid].data;
+            delete self._collectCtx[cid];
+            callback(false, data);
+        }, opts.timeout);
+    }
+    this.broadcast(source, opts);
 }
 
 Twst.prototype.on = function(type, callback) {
